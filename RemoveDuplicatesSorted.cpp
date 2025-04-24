5f4a8d74c3b3a6d81a77e213812e67cf1c1a78be
Paweł Skotnicki <75095484+skot03@users.noreply.github.com>
2025-04-24 19:42:51 +0200

    Update RemoveDuplicatesSorted.cpp
    test added

--- RemoveDuplicatesSorted.cpp
+++ RemoveDuplicatesSorted.cpp
@@ -12,3 +12,13 @@ public:
         return i+1;
     }
 };
+//solution tested with the following code from leetcode
+/*int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}*/
